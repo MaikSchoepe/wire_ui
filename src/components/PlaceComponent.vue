dc51700e31707bdf8d4a926d9d145db6118ce17f
Maik Schöpe <m.schoepe@ccgmbh.de>
2022-05-15 00:11:00 +0200

    added places table

---
+++ src/components/PlaceComponent.vue
@@ -0,0 +1,92 @@
+<template>
+  <q-page class="q-pa-md row items-stretch">
+    <div v-if="loading">Loading...</div>
+    <div v-else-if="error">Error: {{ error.message }}</div>
+    <div v-else-if="result && result.places">
+      <q-table
+        title="Räume"
+        :rows="result.places"
+        :columns="columns"
+        row-key="id"
+        v-model:pagination="pagination"
+        :loading="loading"
+        :filter="filter"
+      ></q-table>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { useQuery } from '@vue/apollo-composable';
+import gql from 'graphql-tag';
+import { QTableProps } from 'quasar';
+
+const columns: QTableProps['columns'] = [
+  {
+    name: 'areaName',
+    label: 'Bereich',
+    field: (row: any) => row.parentArea.shortName,
+    sortable: true,
+    align: 'left',
+  },
+  { name: 'id', label: 'ID', field: 'id', sortable: true, align: 'left' },
+  {
+    name: 'shortName',
+    label: 'Kurzname',
+    field: 'shortName',
+    sortable: true,
+    align: 'left',
+  },
+  {
+    name: 'name',
+    label: 'Name',
+    field: 'name',
+    sortable: true,
+    align: 'left',
+  },
+  {
+    name: 'description',
+    label: 'Beschreibung',
+    field: 'description',
+    sortable: true,
+    align: 'left',
+  },
+];
+
+export default defineComponent({
+  name: 'PlaceComponent',
+  setup() {
+    const filter: any = ref('');
+    const pagination = ref({
+      sortBy: 'id',
+      descending: false,
+      page: 1,
+      rowsPerPage: 10,
+    });
+
+    const { result, loading, error } = useQuery(gql`
+      {
+        places {
+          id
+          name
+          shortName
+          description
+          parentArea {
+            shortName
+          }
+        }
+      }
+    `);
+
+    return {
+      filter,
+      loading,
+      pagination,
+      columns,
+      result,
+      error,
+    };
+  },
+});
+</script>
--- src/pages/PlacePage.vue
+++ src/pages/PlacePage.vue
@@ -1,14 +1,16 @@
 <template>
   <q-page padding class="row items-center justify-evenly">
-    <h3>Places</h3>
+    <place-component></place-component>
   </q-page>
 </template>
 
 <script lang="ts">
+import PlaceComponent from 'components/PlaceComponent.vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'PlacePage',
+  components: { PlaceComponent },
   setup() {
     return {};
   },
